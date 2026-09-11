635664254e7c9fac018b446c04fd3997fe2c0adb
SaumYadavbot <somubhaiya56@gmail.com>
2026-09-11 18:38:39 +0530

    Update bot.py

--- telewater/bot.py
+++ telewater/bot.py
@@ -72,7 +72,7 @@ def _start_connection_watchdog(client):
 # TEXT WATERMARK SETTINGS
 # =========================================================
 
-WATERMARK_NAME = "."
+WATERMARK_NAME = "JOIN"
 WATERMARK_USERNAME = "@EternalCivilAcademy"
 
 # Approximately 60% opacity
